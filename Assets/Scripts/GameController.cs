4f1a510cad04789cdcb3d2c973a21f8ad4ea087c
HOBIRON <yudainepnep@icloud.com>
2022-02-19 17:03:53 +0900

    refactor: UI周りの処理をUIManagerに移動

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,54 +6,38 @@ using Photon.Realtime;
 using TMPro;
 using UnityEngine;
 
-public class GameController : MonoBehaviourPunCallbacks {
-    public GameObject selectHostOrPlayerUI;
-    public GameObject resultUI;
-    public GameObject InputProfileUI;
-    public GameObject HostStandbyUI;
-
-    public GameObject IntroducePlayerUI;
-    public TextMeshProUGUI IntroduceNameText;
-    public TextMeshProUGUI IntroduceHobbyText;
-
-    public TextMeshProUGUI participantNumText;
-    public TMP_InputField inputFieldName;
+public class GameController : MonoBehaviourPunCallbacks
+{
+    public UIManager UIManager;
 
     public GameObject followPlayerCamera;
 
     public GameObject avatars;
-
     private int participantNum = 0;
-
     private List<GameObject> players = new List<GameObject> ();
 
-    public void OnParticipateAsPlayerButtonPush () {
-        InputProfileUI.SetActive (true);
+    public void GameStart ()
+    {
+        Debug.Log ("Game Start");
     }
 
-    public void OnParticipateAsHostButtonPush () {
-        CreateRoom ();
-        HostStandbyUI.SetActive (true);
-    }
-
-    public void OnInputProfileCompleteButtonPush () {
-        ParticipateRoom ();
-    }
-
-    public void OnPlayerParticipateCompleteButtonPush () {
-        //自己紹介の開始
+    public void StartIntroduction ()
+    {
         photonView.RPC (nameof (IntroducePlayers), RpcTarget.All);
-        HostStandbyUI.SetActive (false);
+
     }
 
     [PunRPC]
-    private void AddPlayerList (GameObject avatar) {
+    private void AddPlayerList (GameObject avatar)
+    {
         avatar.transform.parent = avatars.transform;
     }
 
-    void InstatiateCharacter () {
+    void InstatiateCharacter ()
+    {
         // Debug.Log (PhotonNetwork.CountOfPlayersInRooms);
-        switch (PhotonNetwork.CountOfPlayersInRooms) {
+        switch (PhotonNetwork.CountOfPlayersInRooms)
+        {
             case 1:
                 GameObject avatar = PhotonNetwork.Instantiate ("Avatar1", new Vector3 (-1.0f, 0.1f, 0f), Quaternion.Euler (-90, 180, 0));
                 followPlayerCamera.GetComponent<CinemachineVirtualCamera> ().Follow = avatar.transform;
@@ -68,80 +52,94 @@ public class GameController : MonoBehaviourPunCallbacks {
         }
     }
 
-    public void CreateRoom () {
+    public void CreateRoom ()
+    {
         PhotonNetwork.NickName = "Host";
         PhotonNetwork.ConnectUsingSettings ();
     }
 
-    public void ParticipateRoom () {
-        PhotonNetwork.NickName = inputFieldName.text;
+    public void ParticipateRoom ()
+    {
+        // PhotonNetwork.NickName = inputFieldName.text;
+        PhotonNetwork.NickName = UIManager.GetComponent<UIManager> ().inputFieldName.text;
         PhotonNetwork.ConnectUsingSettings ();
     }
 
-    public override void OnConnectedToMaster () {
+    public override void OnConnectedToMaster ()
+    {
         // ランダムなルームに参加する
         PhotonNetwork.JoinRandomRoom ();
     }
 
     // ランダムで参加できるルームが存在しないなら、新規でルームを作成する
-    public override void OnJoinRandomFailed (short returnCode, string message) {
+    public override void OnJoinRandomFailed (short returnCode, string message)
+    {
         var roomOptions = new RoomOptions ();
         roomOptions.MaxPlayers = 3;
         PhotonNetwork.CreateRoom (null, roomOptions);
     }
 
-    public override void OnJoinedRoom () {
+    public override void OnJoinedRoom ()
+    {
         //マスターは傍観者
-        if (!PhotonNetwork.IsMasterClient) {
+        if (!PhotonNetwork.IsMasterClient)
+        {
             InstatiateCharacter ();
         }
 
-        selectHostOrPlayerUI.SetActive (false);
-        InputProfileUI.SetActive (false);
+        UIManager.GetComponent<UIManager> ().selectHostOrPlayerUI.SetActive (false);
+        UIManager.GetComponent<UIManager> ().InputProfileUI.SetActive (false);
     }
 
-    public void Goal () {
-        resultUI.SetActive (true);
+    public void Goal ()
+    {
+        UIManager.GetComponent<UIManager> ().resultUI.SetActive (true);
     }
 
     [PunRPC]
-    public void IntroducePlayers () {
-        foreach (var photonView in PhotonNetwork.PhotonViewCollection) {
+    public void IntroducePlayers ()
+    {
+        foreach (var photonView in PhotonNetwork.PhotonViewCollection)
+        {
             Debug.Log ($"{photonView.gameObject.tag}({photonView.ViewID})");
-            if (photonView.gameObject.tag == "Player") {
+            if (photonView.gameObject.tag == "Player")
+            {
                 players.Add (photonView.gameObject);
             }
         }
 
-        StartCoroutine ("StartIntoroduction");
+        StartCoroutine ("StartIntoroductionCoroutine");
     }
 
-    public void UpdateParticipantNum () {
+    public void UpdateParticipantNum ()
+    {
         participantNum++;
-        participantNumText.text = $"現在 {participantNum} 人";
+        UIManager.participantNumText.text = $"現在 {participantNum} 人";
     }
 
-    IEnumerator StartIntoroduction () {
-
-        for (int i = 0; i < players.Count; i++) {
+    IEnumerator StartIntoroductionCoroutine ()
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
             var player = GameObject.Find ($"Avatar{i+1}(Clone)");
             player.transform.Find ("VCam").gameObject.GetComponent<CinemachineVirtualCamera> ().enabled = true;
             yield return new WaitForSeconds (2);
 
             player.GetComponent<AvatarController> ().HideMyNameFromOthers ();
-            IntroducePlayerUI.SetActive (true);
-            IntroduceNameText.text = player.GetComponent<AvatarController> ().returnName ();
+            UIManager.IntroducePlayerUI.SetActive (true);
+            UIManager.IntroduceNameText.text = player.GetComponent<AvatarController> ().returnName ();
 
             yield return new WaitForSeconds (5);
             player.GetComponent<AvatarController> ().InformNameToOthers ();
         }
 
-        foreach (var player in players) {
+        UIManager.IntroducePlayerUI.SetActive (false);
+
+        foreach (var player in players)
+        {
             player.transform.Find ("VCam").gameObject.GetComponent<CinemachineVirtualCamera> ().enabled = false;
+            // player.GetComponent<AvatarController> ().InformNameToOthers ();
         }
 
-        IntroducePlayerUI.SetActive (false);
-
     }
-
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,24 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour
+{
     public GameObject GameController;
+    GameController gameController;
 
     public GameObject selectHostOrPlayerUI;
     public GameObject resultUI;
     public GameObject InputProfileUI;
+    public GameObject HostStandbyUI;
 
-    // Start is called before the first frame update
-    void Start () {
+    public GameObject IntroducePlayerUI;
+    public TextMeshProUGUI IntroduceNameText;
+    public TextMeshProUGUI IntroduceHobbyText;
 
+    public TextMeshProUGUI participantNumText;
+    public TMP_InputField inputFieldName;
+
+    public GameObject countDownUI;
+    public TextMeshProUGUI countDownText;
+
+    void Start ()
+    {
+        gameController = GameController.GetComponent<GameController> ();
     }
 
-    public void OnParticipateAsPlayerButtonPush () {
+    // Start is called before the first frame update
+    public void OnParticipateAsPlayerButtonPush ()
+    {
         InputProfileUI.SetActive (true);
     }
 
-    public void OnInputProfileCompleteButtonPush () {
-        GameController.GetComponent<GameController> ().ParticipateRoom ();
+    public void OnParticipateAsHostButtonPush ()
+    {
+        gameController.CreateRoom ();
+        HostStandbyUI.SetActive (true);
+    }
+
+    public void OnInputProfileCompleteButtonPush ()
+    {
+        gameController.ParticipateRoom ();
+    }
+
+    public void OnPlayerParticipateCompleteButtonPush ()
+    {
+        //自己紹介の開始
+        gameController.StartIntroduction ();
+        HostStandbyUI.SetActive (false);
+    }
+
+    IEnumerator CountDownCoroutine ()
+    {
+        countDownUI.SetActive (true);
+        yield return new WaitForSeconds (1.5f);
+
+        countDownText.text = "3";
+        yield return new WaitForSeconds (1.0f);
+
+        countDownText.text = "2";
+        yield return new WaitForSeconds (1.0f);
+
+        countDownText.text = "1";
+        yield return new WaitForSeconds (1.0f);
+
+        // countDown.text = "スタート";
+        yield return new WaitForSeconds (1.0f);
+
+        // countDownUI.SetActive (false);
+        gameController.GameStart ();
     }
 }
